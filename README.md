ae5d1cf11574b99af743b36c2874e1ac8c54daca
An <1633004849@qq.com>
2021-10-18 11:03:51 +0800

    第二版

--- README.md
+++ README.md
@@ -1,25 +1,35 @@
-安装依赖
+# 安装依赖
 ```
 pip install -r requriements.txt
 ```
-运行
+# 运行
 ```
 python main.py
 ```
-说明
+# 说明
 主要看 readExcel 方法
 ```
 workBook = xlrd.open_workbook(path + '\\' + '商品列表.xls')
 ```
 修改这行代码的 '商品列表.xls'
+```
+# requestT1通过（tiaoma.cnaidc.com）查询商品信息
+# requestT1('6935284412918') # 固定测试
+requestT1(shop_id)
 
-request方法访问接口
-request('6935284412918') # 固定测试
-request(shop_id)
-
-getBaiDu方法访问百度接口
-getBaiDu('6903148094501', '矿泉水') # 固定测试
+# getBaiDu方法访问百度接口
+# getBaiDu('6903148094501', '矿泉水') # 固定测试
 getBaiDu(shop_id, shop_name)
-dowmloadPic方法里面的num_download变量设置下载图片数量
+# dowmloadPic 方法里面的 num_download 变量 设置下载图片数量
 
-requestShowApi方法是访问易源的接口(收费)
+# requestT2 中国物品编码
+# getBaiDu('6903148094501') # 固定测试 
+requestT2(shop_id)
+'''
+使用方法
+ 1.先来到中国物品编码（http://www.ancc.org.cn/Service/queryTools/Internal.aspx）
+ 2.填写验证码
+ 3.网络抓包拿到cookie
+ 4.填写进该项目 requestT2 里面的 headers['Cookie'] = 'ASP.NET_SessionId=blgmvuf5s54mtz45si25rga2' # 需要手动获取
+ '''
+ ```
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 
+from logging import fatal
 import ddddocr
 import requests
 import json
@@ -10,12 +11,13 @@ import xlrd
 import threading
 import re
 
+from bs4 import BeautifulSoup
 
 from msg_logger import logger
 
 
-url = 'http://tiaoma.cnaidc.com'
-headers = {'User-Agent':'Mozilla/5.0'}
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
 
 path = os.path.abspath(os.path.dirname(sys.argv[0]))
 
@@ -25,8 +27,27 @@ def parse_json(s):
     end = s.rfind('}') + 1
     return json.loads(s[begin:end])
 
-# 请求查询商品
-def request(shop_id):
+# 创建目录
+def mkdir(path):
+     # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+    # 判断路径是否存在
+    isExists=os.path.exists(path)
+    # 判断结果
+    if not isExists:
+        os.makedirs(path)
+        logger.info(path + ' 创建成功')
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        logger.info(path + ' 目录已存在')
+        return False
+
+# 爬取 "tiaoma.cnaidc.com" 来查找商品信息
+def requestT1(shop_id):
+    url = 'http://tiaoma.cnaidc.com'
     s = requests.session()
 
     # 获取验证码
@@ -67,25 +88,8 @@ def request(shop_id):
             logger.info('访问图片URL出现错误！') 
        
     if resp_json['msg'] == '验证码错误':
-        request(shop_id)
+        requestT1(shop_id)
 
-# 创建目录
-def mkdir(path):
-     # 去除首位空格
-    path = path.strip()
-    # 去除尾部 \ 符号
-    path = path.rstrip("\\")
-    # 判断路径是否存在
-    isExists=os.path.exists(path)
-    # 判断结果
-    if not isExists:
-        os.makedirs(path)
-        logger.info(path + ' 创建成功')
-        return True
-    else:
-        # 如果目录存在则不创建，并提示目录已存在
-        logger.info(path + ' 目录已存在')
-        return False
 
 # 通过配合商品名通过百度找图片
 def getBaiDu(shop_id, search_title):
@@ -117,32 +121,47 @@ def dowmloadPic(html, shop_id):
         num_download = num_download - 1
         logger.info(path + '\\' + shop_id + '\\' + str(localtime) +'.png')
 
-# 去水印
-def drop_wartermark(path):
-    print('因为找不到合适的库, 就没有对接')
 
-# 调用万维容源api
-def requestShowApi(shop_id):
-    shop_time = int(time.time())
-    appid = '791873'
-    sign = 'b84e7b3717df4f18a4f8380707ef6663'
-    showapi_url = 'https://route.showapi.com/66-22?code='+shop_id+'&showapi_appid='+appid+'&showapi_timestamp='+str(shop_time)+'&showapi_sign='+sign
-    resp = s.get(showapi_url, headers=headers)
-    resp_json = parse_json(resp.text)
-    logger.info(resp_json)
-    print(resp_json['showapi_res_body'])
-     # 判断是否查询成功
-    if str(resp_json['showapi_res_code']) == '0' and resp_json['showapi_res_body'].get('img'):
-        # 保存商品图片
-        img_url = resp_json['showapi_res_body']['img']
-        shop_img_data  = s.get(img_url,  headers=headers).content
-        # 新建目录
-        mkdir(path + '\\' + shop_id)
-        localtime = time.strftime("%Y%m%d%H%M%S", time.localtime())
-        # 保存图片
-        with open(path + '\\' + shop_id + '\\' + str(localtime) +'.png','wb') as v:
-            v.write(shop_img_data)
-        logger.info(path + '\\' + shop_id + '\\' + str(localtime) +'.png')
+# 中国物品编码
+def requestT2(shop_id):
+    s = requests.session()
+    t2_url = 'http://search.anccnet.com/searchResult2.aspx?keyword='+ shop_id
+    headers['Cookie'] = 'ASP.NET_SessionId=blgmvuf5s54mtz45si25rga2' # 需要手动获取
+    headers['Host'] = 'search.anccnet.com'
+    headers['Referer'] = 'http://search.anccnet.com/searchResult2.aspx'
+    resp = s.get(t2_url, headers=headers)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    results = soup.find(attrs={"id":"results"})
+    if results:
+        for div_tag in results.find_all('div', {'class': 'result'}):
+            p_info = div_tag.find('dl', {'class': 'p-info'})
+            dd = p_info.find_all('dd')
+            shop_name = dd[3].text
+            a = dd[0].find('a')
+            # 获取商品详情的大图
+            shop_resp = s.get(a['href'], headers=headers)
+            shop_soup = BeautifulSoup(shop_resp.text, 'lxml')
+            results_img = shop_soup.find(attrs={"id":"imageListDiv"})
+            if results_img:
+                # 新建目录
+                mkdir(path + '\\' + shop_id)
+                for img_tag in results_img.find_all('img'):
+                    try:
+                        pic = requests.get(img_tag['src'], timeout=10, headers=headers)
+                    except requests.exceptions.ConnectionError:
+                        logger.info('访问商品图片出现错误！')
+                        continue
+                    localtime = time.strftime("%Y%m%d%H%M%S", time.localtime())
+                    fn = open(path + '\\' + shop_id + '\\' + str(localtime) +'.png','wb')
+                    fn.write(pic.content)
+                    fn.close()
+                    logger.info(path + '\\' + shop_id + '\\' + str(localtime) +'.png')
+            else:
+                logger.info('条码：{0} 商品名：{1} 当前抓取商品无图片！'.format(shop_id, shop_name))
+    else:
+        logger.info('当前访问过快啦~中国物品编码拒绝了我们的访问, 请等待30秒在访问')
+        time.sleep(30)
+        requestT2(shop_id)
 
 
 def run(sheet1_content1, start_num, end_num):
@@ -152,24 +171,29 @@ def run(sheet1_content1, start_num, end_num):
         shop_id = str(rows_str)
         # print(shop_id)
         shop_name = sheet1_content1.cell(i, 0).value
+        # 判断开头是6开始的
         if shop_id.startswith('6'):
-            if len(shop_id) == 13 or len(shop_id) == 8:
+            # 判断长度为13或者14
+            if len(shop_id) == 13 or len(shop_id) == 14:
                 logger.info('开始执行 ' + str(i) + ' 个' + ' 商品id ' + shop_id + ' 商品名 ' + shop_name)
                 
                 # 从哪里中断就从哪里开始执行
                 # if i >= 370:
-                    # request('6935284412918')
-                    # request(shop_id)
+                    # requestT1(shop_id)
+                    # requestT2(shop_id)
 
-                # request('6935284412918')
-                # request(shop_id)
-            
+                # 通过 "tiaoma.cnaidc.com" 查找
+                # requestT1('6935284412918')
+                # requestT1(shop_id)
+
+                # 通过百度查找
                 # getBaiDu('6903148094501', '矿泉水')
-                getBaiDu(shop_id, shop_name)
+                # getBaiDu(shop_id, shop_name)
 
-                # requestShowApi('6956367338666')
-                # requestShowApi(shop_id)
-                # time.sleep(0.5)
+                # 通过 中国物品编码 查找
+                # requestT2('6900082020184')
+                requestT2(shop_id)
+                   
 
 # 读取exec进行循环访问
 def readExcel():
@@ -178,15 +202,14 @@ def readExcel():
     sheet1_content1 = workBook.sheet_by_index(0)
     # 获取总行数
     nrows = sheet1_content1.nrows
-    work_count = 5
-    num = int(nrows / work_count)
     start_num = 1
-    end_num = 0
-
-    end_num = 600
+    # end_num = 0
+    end_num = nrows
 
     run(sheet1_content1, start_num, end_num)
-
+    # 开启多线程查找
+    # work_count = 5
+    # num = int(nrows / work_count)
     # for i in range(work_count):
     #     if i == 0:
     #         start_num = 1
@@ -202,12 +225,6 @@ def readExcel():
     #         thread_seckill.start()
 
 
-def t():
-    workBook = xlrd.open_workbook(path + '\\' + '商品列表.xls')
-    sheet1_content1 = workBook.sheet_by_index(0)
-    # 获取总行数
-    nrows = sheet1_content1.nrows
-    run(sheet1_content1, 1, nrows)
 
 if __name__ == '__main__':
     readExcel()
